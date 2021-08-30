6022331524577258a20299dd49d0ce5c70ca5765
Yehuda Rimon <yehudareisler@gmail.com>
2021-08-30 15:45:59 +0300

    Create board_utilities.py

---
+++ board_utilities.py
@@ -0,0 +1,20 @@
+from territory import Territory
+from player import Player
+from continent import Continent
+
+
+class BoardUtils:
+    # @staticmethod
+    # def get_distance(self, source: Territory, target: Territory):
+    #     if source.ruler == target.ruler:
+    #         return float("inf")
+    @staticmethod
+    def get_continent_ratio(source: Continent, player: Player):
+        player_units, enemy_units = 0, 0
+        for territory in source.territories:
+            if territory.ruler == player:
+                player_units += territory.troops
+            else:
+                enemy_units += territory.troops
+        print(player_units,enemy_units)
+        return player_units / enemy_units if enemy_units != 0 else float("inf")
